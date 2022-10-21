c728ee3a1a970efadc141f80756f6440fc85a43d
UntungJamari <untunggjamari@gmail.com>
2022-10-21 10:33:45 +0700

    detail ppiu

--- kab_kota/dashboard_kab_kota.php
+++ kab_kota/dashboard_kab_kota.php
@@ -63,7 +63,7 @@ include "session_kab_kota.php";
                                 </div>
                                 <!-- Card Body -->
                                 <div class="card-body">
-                                    <a href="tambah_ppiu.php" type="button" class="btn btn-outline-info mb-3">
+                                    <a href="tambah_ppiu.php" type="button" class="btn btn-outline-info btn-sm mb-3">
                                         <i class="fas fa-fw fa fa-plus"></i>
                                         <span>Tambah PPIU</span>
                                     </a>
@@ -82,8 +82,9 @@ include "session_kab_kota.php";
                                                     <th>Nama PPIU</th>
                                                     <th>Status</th>
                                                     <th>No. SK</th>
-                                                    <th style="width: 15%;">Tanggal SK</th>
+                                                    <th style="width: 13%;">Tanggal SK</th>
                                                     <th>Alamat</th>
+                                                    <th style="width: 14%;">Aksi</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -99,6 +100,17 @@ include "session_kab_kota.php";
                                                         <td><?php echo $tampil['nomor_sk']; ?></td>
                                                         <td><?php echo date('d-m-Y', strtotime($tampil['tanggal_sk'])); ?></td>
                                                         <td><?php echo $tampil['alamat']; ?></td>
+                                                        <td>
+                                                            <a id="detail" type="button" class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#modal-detail" data-id_ppiu="<?php echo $tampil['id_ppiu']; ?>" data-nama_ppiu="<?php echo $tampil['nama_ppiu']; ?>" data-nama_kab_kota="<?php echo $tampil['nama_kab_kota']; ?>" data-status="<?php echo $tampil['status']; ?>" data-nomor_sk="<?php echo $tampil['nomor_sk']; ?>" data-tanggal_sk="<?php echo $tampil['tanggal_sk']; ?>" data-alamat="<?php echo $tampil['alamat']; ?>" data-nama_pimpinan="<?php echo $tampil['nama_pimpinan']; ?>" data-logo="<?php echo $tampil['logo']; ?>">
+                                                                <i class="fas fa-fw fa fa-eye"></i>
+                                                            </a>
+                                                            <a href="edit_ppiu.php?id_ppiu=<?php echo $tampil['id_ppiu'] ?>" type="button" class="btn btn-outline-warning btn-sm">
+                                                                <i class="fas fa-fw fa fa-edit"></i>
+                                                            </a>
+                                                            <a href="dashboard_kab_kota.php?id_ppiu=<?php echo $tampil['id_ppiu'] ?>" type="button" class="btn btn-outline-danger btn-sm">
+                                                                <i class="fas fa-fw fa fa-trash-alt"></i>
+                                                            </a>
+                                                        </td>
                                                     </tr>
                                                 <?php
                                                     $no++;
@@ -113,7 +125,85 @@ include "session_kab_kota.php";
                         </div>
                     </div>
                     <!-- /.container-fluid -->
+                    <div class="modal fade" id="modal-detail">
+                        <div class="modal-dialog">
+                            <div class="modal-content card shadow mb-4">
+                                <div class="modal-header card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h4 class="modal-title m-0 font-weight-bold text-primary">Detail PPIU</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body table-responsive card-body">
+                                    <center>
+                                        <img class="mb-5" id="gambar" style="width: 300px;">
+                                    </center>
+                                    <table class="table table-bordered no-margin">
+                                        <tbody>
+                                            <tr>
+                                                <th>ID PPIU</th>
+                                                <td><span id="id_ppiu"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Nama PPIU</th>
+                                                <td><span id="nama_ppiu"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Kab/Kota</th>
+                                                <td><span id="nama_kab_kota"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Status</th>
+                                                <td><span id="status"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Nomor SK</th>
+                                                <td><span id="nomor_sk"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Tanggal SK</th>
+                                                <td><span id="tanggal_sk"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Alamat</th>
+                                                <td><span id="alamat"></span></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Nama Pimpinan</th>
+                                                <td><span id="nama_pimpinan"></span></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <script>
+                        $(document).ready(function() {
+                            $(document).on('click', '#detail', function() {
+                                var id_ppiu = $(this).data('id_ppiu');
+                                var nama_ppiu = $(this).data('nama_ppiu');
+                                var nama_kab_kota = $(this).data('nama_kab_kota');
+                                var status = $(this).data('status');
+                                var nomor_sk = $(this).data('nomor_sk');
+                                var tanggal_sk = $(this).data('tanggal_sk');
+                                var alamat = $(this).data('alamat');
+                                var nama_pimpinan = $(this).data('nama_pimpinan');
+                                var logo = $(this).data('logo');
+                                $('#id_ppiu').text(id_ppiu);
+                                $('#nama_ppiu').text(nama_ppiu);
+                                $('#nama_kab_kota').text(nama_kab_kota);
+                                $('#status').text(status);
+                                $('#nomor_sk').text(nomor_sk);
+                                $('#tanggal_sk').text(tanggal_sk);
+                                $('#alamat').text(alamat);
+                                $('#nama_pimpinan').text(nama_pimpinan);
+                                $('#logo').text(logo);
+                                $('#gambar').attr('src', '../images/profile/' + logo);
 
+                            })
+                        })
+                    </script>
                 </div>
                 <!-- End of Main Content -->
 
@@ -126,7 +216,6 @@ include "session_kab_kota.php";
                     </div>
                 </footer>
                 <!-- End of Footer -->
-
             </div>
             <!-- End of Content Wrapper -->
 
--- kab_kota/header.php
+++ kab_kota/header.php
@@ -1,3 +1,10 @@
+<?php
+
+$username = $_SESSION['username'];
+$query = mysqli_query($koneksi, "select * from kemenag_kab_kota where username='$username'");
+$result = mysqli_fetch_assoc($query);
+
+?>
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
@@ -19,11 +26,11 @@
                     echo $username_h;
                     ?>
                 </span>
-                <img class="img-profile rounded-circle" src="../images/profile/default.png">
+                <img class="img-profile rounded-circle" src="../images/<?php echo $result['logo']; ?>">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="profile_lihat.php">
+                <a class="dropdown-item" href="lihat_profil.php">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
